03ad8473e5e7909111fc6984c098c61cf8d3234b
adlaideduah-maker <adlaideduah@gmail.com>
2026-02-16 09:50:49 +0000

    Update app.js

--- translation-engine/app.js
+++ translation-engine/app.js
@@ -99,6 +99,6 @@ app.post("/translate", upload.single("file"), async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("SayBon Engine running on port " + PORT);
 });
