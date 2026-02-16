fae57f379606cf1c559d8fa69edcd9dff04cef1e
adlaideduah-maker <adlaideduah@gmail.com>
2026-02-16 12:20:48 +0000

    Update app.js

--- translation-engine/app.js
+++ translation-engine/app.js
@@ -50,9 +50,6 @@ app.post("/api/translate", upload.single("file"), async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
-
 });
-
