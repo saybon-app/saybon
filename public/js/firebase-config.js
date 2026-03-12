2be985ee405bc4a8e783251283d28b3d3abf220b
adelaideduah-maker <adlaideduah@gmail.com>
2026-03-12 21:31:20 +0000

    Add SayBon translator application recording system

---
+++ public/js/firebase-config.js
@@ -0,0 +1,14 @@
+﻿import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
---
+++ public/translation/translator-test.js
@@ -0,0 +1,44 @@
+﻿import { db } from "../js/firebase-config.js";
+
+import {
+doc,
+updateDoc
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+
+const params = new URLSearchParams(window.location.search);
+const applicantId = params.get("applicant");
+
+
+window.submitTest = async function(){
+
+const english =
+document.getElementById("englishBox").value;
+
+const french =
+document.getElementById("frenchBox").value;
+
+if(!english || !french){
+
+alert("Please complete both translations");
+
+return;
+
+}
+
+await updateDoc(
+doc(db,"translatorApplications", applicantId),
+{
+
+englishTranslation: english,
+frenchTranslation: french,
+
+testCompleted:true,
+submittedAt: Date.now()
+
+});
+
+window.location.href =
+"/translation/translator-register.html";
+
+}
