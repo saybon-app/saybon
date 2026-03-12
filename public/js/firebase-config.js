7b344e143177a3d240cd6bb85ffe98a4da22a8ea
adelaideduah-maker <adlaideduah@gmail.com>
2026-03-12 21:32:53 +0000

    Add SayBon Firebase config for translator system

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -2,13 +2,17 @@
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+
+apiKey: "AIzaSyB2aKUdE1NSt0kN332BwTYSX52D0lxj1g0",
+authDomain: "saybon-3e3c2.firebaseapp.com",
+projectId: "saybon-3e3c2",
+storageBucket: "saybon-3e3c2.firebasestorage.app",
+messagingSenderId: "75085012344",
+appId: "1:75085012344:web:0b18581cb0a30c3df47c8d"
+
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
+
