c087b421cb0dd4ba8ee962bff8a9972fd6c415d9
adelaideduah-maker <adlaideduah@gmail.com>
2026-04-21 20:32:37 +0000

    Polish access screens with smoother premium animation and smaller scale

--- public/admin/access-granted/access-granted.js
+++ public/admin/access-granted/access-granted.js
@@ -1,5 +1,17 @@
 ﻿(function () {
+  const shell = document.getElementById("screenShell");
+
+  requestAnimationFrame(() => {
+    requestAnimationFrame(() => {
+      shell.classList.remove("is-entering");
+    });
+  });
+
+  setTimeout(() => {
+    shell.classList.add("is-leaving");
+  }, 1350);
+
   setTimeout(() => {
     window.location.href = "/features/admin/";
-  }, 1700);
+  }, 1850);
 })();
