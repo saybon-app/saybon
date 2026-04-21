7e5d314b9016fa0c6eaff9c5da2b0f680411d327
adelaideduah-maker <adlaideduah@gmail.com>
2026-04-21 20:32:37 +0000

    Polish access screens with smoother premium animation and smaller scale

--- public/admin/access-denied/access-denied.js
+++ public/admin/access-denied/access-denied.js
@@ -1,5 +1,17 @@
 ﻿(function () {
+  const shell = document.getElementById("screenShell");
+
+  requestAnimationFrame(() => {
+    requestAnimationFrame(() => {
+      shell.classList.remove("is-entering");
+    });
+  });
+
+  setTimeout(() => {
+    shell.classList.add("is-leaving");
+  }, 950);
+
   setTimeout(() => {
     window.location.href = "/admin/passkey/";
-  }, 1300);
+  }, 1450);
 })();
