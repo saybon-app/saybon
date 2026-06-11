3db980c19177b7d32d9d73820eeea754ed2b7952
adelaideduah-maker <adlaideduah@gmail.com>
2026-06-11 12:17:36 +0000

    Add guide pose animation cycle

---
+++ public/talkletics/guide-animation.js
@@ -0,0 +1,31 @@
+﻿const guide = document.querySelector(".guide");
+
+if (guide) {
+
+  const poses = [
+    "/assets/talkletics/guide.png",
+    "/assets/talkletics/guide-blink.png",
+    "/assets/talkletics/guide.png",
+    "/assets/talkletics/guide-wave.png",
+    "/assets/talkletics/guide.png",
+    "/assets/talkletics/guide-wink.png",
+    "/assets/talkletics/guide.png",
+    "/assets/talkletics/guide-salute.png",
+    "/assets/talkletics/guide.png"
+  ];
+
+  let frame = 0;
+
+  setInterval(() => {
+
+    frame++;
+
+    if(frame >= poses.length){
+      frame = 0;
+    }
+
+    guide.src = poses[frame];
+
+  }, 2200);
+
+}
